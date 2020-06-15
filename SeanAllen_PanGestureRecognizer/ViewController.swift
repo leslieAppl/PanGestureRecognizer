6d0614dbc1e67836a1df2ed4d5c2fee243f80279
Leslie <leslie.appl@gmail.com>
2020-06-15 22:55:30 +0800

    move view with pan finished

--- SeanAllen_PanGestureRecognizer/ViewController.swift
+++ SeanAllen_PanGestureRecognizer/ViewController.swift
@@ -10,11 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fileImageView: UIImageView!
+    @IBOutlet weak var trashImageView: UIImageView!
+    
+    var fileViewOrigin: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        addPanGesture(view: fileImageView)
+        fileViewOrigin = fileImageView.frame.origin
+        
+        view.bringSubviewToFront(fileImageView)
     }
 
-
+    func addPanGesture(view: UIView) {
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(self.handlePan(sender:)))
+        view.addGestureRecognizer(pan)
+        
+    }
+    
+    @objc func handlePan(sender: UIPanGestureRecognizer) {
+        
+        let fileView = sender.view!
+        let translation = sender.translation(in: view)
+        
+        switch sender.state {
+        case .began, .changed:
+            
+            /// Tracking target view's routes corresponding to the pan gesture translation in the coordinate system of the specified view.
+            fileView.center = CGPoint(x: fileView.center.x+translation.x, y: fileView.center.y+translation.y)
+            
+            /// Resetting translation value to zero after pan gesture finished.
+            sender.setTranslation(CGPoint.zero, in: view)
+            
+        case .ended:
+            
+            if fileView.frame.intersects(trashImageView.frame) {
+                
+                UIView.animate(withDuration: 0.0) {
+                    self.fileImageView.alpha = 0.0
+                    self.trashImageView.image = UIImage(named: "fullRecycleBinIcon")
+                }
+            }
+            else {
+                UIView.animate(withDuration: 0.3) {
+                    fileView.frame.origin = self.fileViewOrigin
+                }
+            }
+            
+            
+        default:
+            break
+        }
+        
+    }
 }
 
